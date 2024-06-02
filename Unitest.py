f1bc3c28573a90392d9d54c993d8195697821904
shirel1233 <155564582+shirel1233@users.noreply.github.com>
2024-06-02 13:13:14 +0300

    Update Unitest.py

--- Unitest.py
+++ Unitest.py
@@ -1,45 +1,55 @@
 import unittest
-from QUIC_Client import QUIC_Client  # Assuming QUIC_Client.py contains the ClientQUIC class
-from QUIC_Server import QUIC_Server  # Assuming QUIC_Server.py contains the ServerQUIC class
-# from QUIC_API import *
+from QUIC_Client import QUIC_Client
+from QUIC_Server import QUIC_Server
 import threading
+import os
 import time
 
-class TestQUICFileTransfer(unittest.TestCase):
+class TestQUICProtocol(unittest.TestCase):
+    SERVER_PORT = 12000
+    SERVER_ADDRESS = ('localhost', SERVER_PORT)
+    ORIGINAL_FILE = 'original_file.bin'
+
+    @classmethod
+    def setUpClass(cls):
+        # Create a large file for testing
+        with open(cls.ORIGINAL_FILE, 'wb') as f:
+            f.write(b'\0' * (10 * 1024 * 1024))  # 10MB of null bytes
+
+        # Start the server in a separate thread
+        cls.server_thread = threading.Thread(target=cls.start_server)
+        cls.server_thread.start()
+        # Give the server a moment to start up
+        time.sleep(1)
+
+    @classmethod
+    def tearDownClass(cls):
+        # Make sure the server thread is finished
+        cls.server_thread.join()
+        # Clean up the original file
+        os.remove(cls.ORIGINAL_FILE)
+
+    @classmethod
+    def start_server(cls):
+        server = QUIC_Server(cls.SERVER_PORT)
+        server.start_server()
+
     def test_file_transfer(self):
-        serverName = 'localhost'
-        serverPort = 12000
-        # Instantiate actual ServerQUIC and ClientQUIC objects
-        server_quic = QUIC_Server(serverPort)
-        client_quic = QUIC_Client(serverName, serverPort)
+        client = QUIC_Client('localhost', self.SERVER_PORT)
+        client.start_client()
 
-        # Run the server in a separate thread
-        # Run the server
-        server_quic.start_server()
+        # Check if the file was received
+        self.assertTrue(os.path.exists('received_file.bin'))
 
-        server_thread = threading.Thread(target=server_quic.start_server())# Start the server operation
-        server_thread.start()
+        # Check if the size of the received file matches the original file
+        original_file_size = os.path.getsize(self.ORIGINAL_FILE)
+        received_file_size = os.path.getsize('received_file.bin')
+        self.assertEqual(original_file_size, received_file_size)
 
-        # Give the server a moment to start
-        time.sleep(1)
+        # Clean up received file
+        os.remove('received_file.bin')
 
-        # Run the client
-        client_quic = QUIC_Client()  # Instantiate the client object
-        client_quic.start_client()  # Start the client operation
-
-        # Give some time for client-server interaction
-        time.sleep(1)
-
-        # Verify server sent the correct amount of data
-        total_data_sent = server_quic.total_data_sent()  # Implement this method in your ServerQUIC class
-        self.assertEqual(total_data_sent, 10 * 1024 * 1024)
-
-        # Verify client received the correct amount of data
-        total_data_received = client_quic.total_data_received()  # Implement this method in your ClientQUIC class
-        self.assertEqual(total_data_received, 10 * 1024 * 1024)
-
-        # Join the server thread
-        server_thread.join()
 
 if __name__ == '__main__':
     unittest.main()
+
